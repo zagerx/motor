2f37d39a0f214871152631f5fb00899a4b7dc1b6
zhangge <zhangge@syriusrobotics.com>
2025-03-27 16:30:49 +0800

    增加zgm_001,zgm_002板子 zgm_002的LED功能正常

--- boards/arm/zgm_002/board.cmake
+++ boards/arm/zgm_002/board.cmake
@@ -2,9 +2,10 @@
 
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
-board_runner_args(pyocd "--target=stm32g431rbtx")
+board_runner_args(jlink "--device=STM32H723ZG" "--speed=4000")
+board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
